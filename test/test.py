7107c61adae0f6164ff121dea471f6df76c18014
Ahmed Abdelkader <ahmed@cosmos.oden.utexas.edu>
2021-08-18 23:59:23 -0500

    Test for nan pixels & upsampling by 2x

--- test/test.py
+++ test/test.py
@@ -58,7 +58,7 @@ class TestAgainstOpenCV(unittest.TestCase):
             lycon_img = rgb_bgr(lycon.load(mkpath('opencv')))
             np.testing.assert_array_equal(cv_img, lycon_img)
 
-    def test_resize(self):
+    def test_no_nan(self):
         src_img = random_rgb_image()
         images = [src_img,
                   src_img.astype(np.float32),
@@ -67,22 +67,54 @@ class TestAgainstOpenCV(unittest.TestCase):
         new_shapes = [
             # No change
             src_img.shape[:2],
-            # Upsample
+            # Upsample 2x
+            tuple(map(int, np.array(src_img.shape[:2]) * 2)),
+            # Upsample 3x
             tuple(map(int, np.array(src_img.shape[:2]) * 3)),
             # Downsample
-            tuple(map(int, np.array(src_img.shape[:2]) / 2))
+            tuple(map(int, np.array(src_img.shape[:2]) // 2))
         ]
         for img in images:
             for (h, w) in new_shapes:
-                for interp in lycon.Interpolation:
+                for interp in range(4):
                     cv_resized = cv2.resize(img, (w, h), interpolation=interp)
                     lycon_resized = lycon.resize(img, width=w, height=h, interpolation=interp)
-                    np.testing.assert_array_equal(
+                    self.assertTrue(not np.isnan(np.sum(cv_resized)),
+                            'NaN with OpenCV for dtype={}, interp={}, size=({}, {})'.format(
+                                img.dtype, interp, w, h))
+                    self.assertTrue(not np.isnan(np.sum(lycon_resized)),
+                            'NaN with lycon for dtype={}, interp={}, size=({}, {})'.format(
+                                img.dtype, interp, w, h))
+
+    def test_resize(self):
+        src_img = random_rgb_image()
+        images = [
+                  src_img.astype(np.float32),
+                  src_img.astype(np.float64),
+                  src_img.astype(np.int16)]                  
+        new_shapes = [
+            # No change
+            src_img.shape[:2],
+            # Upsample 2x
+            tuple(map(int, np.array(src_img.shape[:2]) * 2)),
+            # Upsample 3x
+            tuple(map(int, np.array(src_img.shape[:2]) * 3)),
+            # Downsample
+            tuple(map(int, np.array(src_img.shape[:2]) // 2))
+        ]
+        for img in images:
+            for (h, w) in new_shapes:
+                for interp in range(4):
+                    cv_resized = cv2.resize(img, (w, h), interpolation=interp)
+                    lycon_resized = lycon.resize(img, width=w, height=h, interpolation=interp)
+                    np.testing.assert_allclose(
                         cv_resized,
                         lycon_resized,
                         err_msg='Mismatch for dtype={}, interp={}, size=({}, {})'.format(
                             img.dtype, interp, w, h
-                        )
+                        ),
+                        rtol=1e-5,
+                        atol=1e-3
                     )
 
 
