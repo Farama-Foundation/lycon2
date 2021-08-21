22f3a5ba93da22a4d550b448fd915fbd0858a850
Ahmed Abdelkader <ahmed@cosmos.oden.utexas.edu>
2021-08-21 05:31:21 -0500

    Case sensitive env var

--- cmake/LocatePythonLibs.cmake
+++ cmake/LocatePythonLibs.cmake
@@ -1,10 +1,10 @@
 # Python executable
 if (NOT DEFINED ${PYTHON_BIN})
   if (DEFINED ENV{pythonlocation})
-    message($ENV{pythonlocation})
+    message($ENV{pythonLocation})
     find_program(PYTHON_BIN python
-                 PATHS $ENV{pythonlocation}
-		 NO_DEFAULT_PATH)
+                 PATHS $ENV{pythonLocation}
+                 NO_DEFAULT_PATH)
     message(${PYTHON_BIN})
   else()
     find_program(PYTHON_BIN python)
