2340f3ec8c859ba332b325a8709265fe7d5fda0c
Ben Black <weepingwillowben@gmail.com>
2021-08-23 22:35:00 -0600

    removed cmake from build script

--- build-wheels.sh
+++ build-wheels.sh
@@ -17,15 +17,6 @@ yum install -y zlib-devel
 yum install -y libjpeg-devel
 yum install -y libpng-devel
 
-
-CMAKE_DIR="/cmk"
-mkdir ${CMAKE_DIR} && cd ${CMAKE_DIR}
-wget --no-check-certificate https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3-Linux-x86_64.tar.gz
-tar -xvf cmake-3.17.3-Linux-x86_64.tar.gz > /dev/null
-mv cmake-3.17.3-Linux-x86_64 cmake-install
-PATH=${CMAKE_DIR}/cmake-install:${CMAKE_DIR}/cmake-install/bin:$PATH
-cd ..
-
 for PYBIN in /opt/python/*/bin; do
     if [[ ( "$PYBIN" == *"36"* ) || ( "$PYBIN" == *"37"* ) || ( "$PYBIN" == *"38"* )  || ( "$PYBIN" == *"39"* ) ]]; then
         "${PYBIN}/pip" install -r /io/requirements.txt
